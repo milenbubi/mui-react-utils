719c734a6499871a019c89dace16e1413d8c4811
Phillip Coitchev <p.coitchev@devrealm.bg>
2026-01-13 13:21:22 +0200

    Add Loader component

--- src/components/C180Loader.tsx
+++ src/components/C180Loader.tsx
@@ -1,61 +1,66 @@
-import { StackProps, LinearProgress, SxProps, Theme } from "@mui/material";
+import { Box, LinearProgress, SxProps, Theme } from "@mui/material";
 import { Centered } from "./Centered";
 
-interface IProps extends StackProps {
+interface IProps {
   open?: boolean;
+  sx?: SxProps<Theme>;
   LPsx?: SxProps<Theme>;
 }
 
 
 
 /**
- * Compact centered loader component for inline or section-level loading states.
- *
- * Displays a horizontally centered `LinearProgress` bar wrapped in a `Centered` layout.
- * Can be customized with additional layout (`sx`) and progress (`LPsx`) styles.
- *
+ * Compact centered loader component for section-level or small-area loading states.
  * Designed for lightweight use cases where a full modal or backdrop is unnecessary.
  *
+ * Displays a horizontally centered `LinearProgress` bart.
+ * Can be customized with additional layout (`sx` and `LPsx`) styles.
+ * 
  * @param {Object} props - Component props.
  * @param {boolean} [props.open=false] - Whether the loader is visible.
- * @param {SxProps<Theme>} [props.sx] - Custom styles for the outer `Centered` container.
+ * @param {SxProps<Theme>} [props.sx] - Custom styles for the wrapper `Box` container.
  * @param {SxProps<Theme>} [props.LPsx] - Custom styles for the inner `LinearProgress` bar.
  *
  * @example
  * ```tsx
  * <C180Loader
  *   open={loading}
- *   sx={{ mt: 3 }}
+ *   sx={{ mb: 3 }}
  *   LPsx={{ bgcolor: "#334", "& .MuiLinearProgress-bar": { bgcolor: "#8ba4c5" } }}
  * />
  * ```
  */
-function C180Loader({ sx, LPsx, open, ...other }: IProps) {
-  if (!open) {
-    return null;
-  }
-
-
+function C180Loader({ open, sx, LPsx }: IProps) {
   return (
-    <Centered
+    <Box
       sx={{
-        px: 5,
-        display: "flex",
-        ...sx,
+        position: "relative",
+        width: 1,
+        height: "1px",
+        ...sx
       }}
-      {...other}
     >
-      <LinearProgress
-        color="inherit"
-        sx={{
-          width: 1,
-          maxWidth: 360,
-          height: 4,
-          borderRadius: "2px",
-          ...LPsx
-        }}
-      />
-    </Centered>
+      {open && (
+        <Centered
+          sx={{
+            px: 5,
+            position: "absolute",
+            left: 0, top: 0, right: 0
+          }}
+        >
+          <LinearProgress
+            color="inherit"
+            sx={{
+              width: 1,
+              maxWidth: 360,
+              height: 2,
+              borderRadius: "1px",
+              ...LPsx
+            }}
+          />
+        </Centered>
+      )}
+    </Box>
   );
 }
 
